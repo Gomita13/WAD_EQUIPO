05c3a6b16d39375a67d74578b560a9f26238601e
saulgame2010 <saulgame2010@gmail.com>
2022-06-01 00:20:54 -0500

    Inico del proyecto
    Se empezó a modelar toda la capa de datos del proyecto

---
+++ ProyectoFinal/src/main/java/com/ipn/mx/datos/Conexion.java
@@ -0,0 +1,49 @@
+package com.ipn.mx.datos;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
+import java.sql.*;
+
+public class Conexion {
+    private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/proyecto?useSSL=false&useTimeZone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String JDBC_USER = "postgres";
+    private static final String JDBC_PASSWORD = "root";
+
+    public static DataSource getDataSource () {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUrl(JDBC_URL);
+        ds.setUsername(JDBC_USER);
+        ds.setPassword(JDBC_PASSWORD);
+        ds.setInitialSize(50);
+        return ds;
+    }
+
+    public static Connection getConnection () throws SQLException {
+        return getDataSource().getConnection();
+    }
+
+    public static void close(ResultSet rs) {
+        try {
+            rs.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+
+    public static void close(PreparedStatement ps) {
+        try {
+            ps.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+
+    public static void close(Connection conn) {
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+}
